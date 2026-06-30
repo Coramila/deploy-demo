982195aa6ac1a70cfa422acd3f0918b1100e815c
Coramila <scarvalho.camiilaa@gmail.com>
2026-06-30 16:59:26 -0300

    primeiro deploy

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Deploy Front-end com Vercel</h1>
+      <p>Minha primeira aplicação publicada em produção.</p>
+    </main>
+  )
+}
+
+export default App
